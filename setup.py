3e4da56cff9f3e962da55fd891d085b868e78e5d
Abhi <cv004700@gmail.com>
2024-06-02 20:44:07 +0530

    Updated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "Abhinandan Kudale",
+    author_email = "cv004700@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
